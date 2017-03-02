551b4d95068090065f33a09a6ebd32d8ba16cb75
Jenny Hamer <jhamer@ucsd.edu>
2017-03-02 11:15:30 -0800

    Thir Party APIs lab

---
+++ public/js/facebook.js
@@ -0,0 +1,19 @@
+function checkLoginState() {
+	FB.getLoginStatus(function(response) {
+		statusChangeCallback(response);
+	});
+}
+
+function statusChangeCallback() {
+	if (response.status === 'connected') {
+		console.log("Successfully logged in via hellish Facebook");
+		FB.api("/me?fields=name,first_name,picture.width(480)", changeUser);
+	}
+}
+
+function changeUser(response) {
+	$(".facebookLogin p").hide();
+	$("#name").text(response.name);
+	$("#photo").replaceWith(response.picture);
+
+}
