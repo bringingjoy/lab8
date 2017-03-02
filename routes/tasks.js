96ff765285bbbcf43be452bf435e370674a440f2
Jenny Hamer <jhamer@ucsd.edu>
2017-03-02 11:58:15 -0800

    Thir Party APIs lab

--- routes/tasks.js
+++ routes/tasks.js
@@ -1,6 +1,14 @@
+var data = {
+	tasks: ['Finish this lab'];
+};
+
+exports.view = function(req, req) {
+	res.render('tasks', data);
+};
+
 exports.addTask = function(req, res) {
 	var taskName = req.body.taskName;
 	console.log(taskName);
 	data.tasks.push(taskName);
 	res.send(taskName);
-}
+};
